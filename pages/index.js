3eb3e94b100baf19839bc6b796f4d66e1e05e5f2
josemiguelq <jose.miguelq95@gmail.com>
2021-01-17 17:31:11 -0300

    redesign select component

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,75 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import CopyButton from "../components/CopyButton";
-import hello from "./api/hello";
+import texts from './../helpers/texts'
+import { render } from 'react-dom';
+import {generate, random} from './../helpers/generateText'
+import React from 'react'
+
 
 function texto() {
-    console.log(hello)
+    console.log(random());
     return 'asdas';
 }
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Você foi metaforado</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-          {/*<select className={styles.select}>*/}
-          {/*    <option>Tristeza</option>*/}
-          {/*    <option>Felicidade</option>*/}
-          {/*</select>*/}
-        <p className={styles.code}>
-            {texto()}
-            <CopyButton/>
-        </p>
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
 
-        <p className={styles.description}>
-            <a> Gerar aleatório</a>
-        </p>
-        <div className={styles.expressionModes}>
-            <p className={styles.mode}>Tristeza</p>
-            <p className={styles.mode}>Felicidade</p>
-            <p className={styles.mode}>Raiva</p>
-            <p className={styles.mode}>Nojo</p>
-        </div>
+    this.state = {
+      analysis: generate()
+    }
+  }
 
-      </main>
+  onClick = () => {
+    this.setState({ analysis: generate() })
+  }
 
-      <footer className={styles.footer}>
-        <p><a href="https://github.com/josemiguelq/vocefoimetaforado" target="_blank">Github</a></p>
-      </footer>
-    </div>
-  )
+  generateSpecific = (key) => {
+    this.setState({ analysis: generate(key) })
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Você foi metaforado</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Você foi metaforado
+          </h1>
+          <p className={styles.analysis}>
+              {this.state.analysis}
+          </p>
+  
+          <div className={styles.description}>
+          <p  onClick={this.onClick}>Gerar aleatório</p>
+
+          <p onClick={this.onClick}>
+              <a>Copiar</a>
+          </p>
+          </div>
+
+          <div className={styles.expressionModes}>
+              <p className={styles.mode} onClick={() => this.generateSpecific('fear')}>Tristeza</p>
+              <p className={styles.mode} onClick={() => this.generateSpecific('fear')}>Medo</p>
+              <p className={styles.mode}>Felicidade</p>
+              <p className={styles.mode}>Raiva</p>
+              <p className={styles.mode}>Nojo</p>
+          </div>
+  
+        </main>
+  
+        <footer className={styles.footer}>
+          <p><a href="https://github.com/josemiguelq/vocefoimetaforado" target="_blank">Github</a></p>
+        </footer>
+      </div>
+    )
+  }
 }
+
+
+export default Home;
