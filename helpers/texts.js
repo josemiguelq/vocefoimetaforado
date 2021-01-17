6ad1d17c50e92b7ae439d37d78087b7f6a565411
josemiguelq <jose.miguelq95@gmail.com>
2021-01-17 19:03:46 -0300

    more analysis

--- helpers/texts.js
+++ helpers/texts.js
@@ -1,22 +1,32 @@
 export default {
         introduction: "Partindo aqui do sistema FACTS, criado na década de 70 por Paul Ekcman",
-        fake: ['Conforme  observado Dr. Paul Ekman apse de latência em até 1seg uma expressão genuína'],
+        fake: [
+            'Conforme  observado Dr. Paul Ekman apse de latência em até 1seg uma expressão genuína',
+            'Podemos observar a linguagem nos ombros, Tensao muscular nos ombros quando esta afirmando algo nao é uma tendencia. Quando acredito no que está falando os gestos sao mais abrangentes.',
+            'Podemos ouvir uma queda no volume local,, é um critério conhecido como V1, quando tem uma queda no volume vocal significa que é algo que a gente nao tem certeza',
+            'Temos um comportamento no ombro, churag',
+            'Claramente vemos gesticulando com a cabeça, fazendo sinal positivo, que sao incongruentes com a narrativa sendo apresentada, isso é chamado de código B1 segundo o protocolo SCANS',
+            'Claramente vemos gesticulando com a cabeça, fazendo sinal negativo, que sao incongruentes com a narrativa sendo apresentada, isso é chamado de código B1 segundo o protocolo SCANS',
+    ],
         happiness: ['Contração bilateral Músculo zigomático maior'],
         fear: [
             'Contracao dos músculos risórios, um AU20',
             'o canto dos lábios se estica lateralmente, uma microexpressao de medo',
 
-            'Alguns artigos falam pra gente como the voice of deception ou a CBCA, a pausa narrativa muito' +
-            ' prolongada pode ocorrer quando a gente nao ter certeza do que a gente tá falando, como podemos ver' +
-            ' aqui neste caso',
+            'Alguns artigos falam pra gente como the voice of deception ou a CBCA, a pausa narrativa muito prolongada pode ocorrer quando a gente nao ter certeza do que a gente tá falando, como podemos ver aqui neste caso',
 
             'Vemos claramente continuas inspiradas fundas e também o canto dos lábios se esticam lateralmente' +
             ' deixando' +
             ' a arcaria dentária lateralmente' +
-            ' exposta, junto com a inspirada fun uma micro expressao facial de medo'
+            ' exposta, junto com a inspirada fun uma micro expressao facial de medo',
+
+            'Vemos uma engolida em seco, como é explicado nas pesquisas de Cliff Lesley e da Emotional Intellegence Academy, quando se está sob efeito do hormonio das emocoes do medo, atecolamina, noradranalinas, e acaba deixando a boca seca. Comportamento muito comum em pessoas nervosas e com medo.'
         ],
         scorn: [
             'a gente observa uma elevacao assimétrica no canto dos lábios'
+        ],
+        sadness: [
+            'Conseguimos observar enrrugamento na regiao do queixo logo abaixo do labio inferior e lábios meia ponta pra baixo, um micro expressao de tristeza, AU15 + 17'
         ],
         anxiety: [
             'Existe uma mudança na linha de base e o fluxo de piscadas está bem elevado e também vemos um aumento na' +
